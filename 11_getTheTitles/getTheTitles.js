26477e38203e80417fc9bb114fe114bd44dcbd80
adam-odin <adam@edtec.io>
2024-09-11 19:53:38 +0100

    Finish exercises

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,30 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    return array.map((books) => books.title);
 };
+
+// const books = [
+//     {
+//       title: 'Book',
+//       author: 'Name'
+//     },
+//     {
+//       title: 'Book2',
+//       author: 'Name2'
+//     }
+//   ]
+
+// const groupByTitle = function(array) {
+//     return array.reduce((object,value) => {
+//         object[value.id] = value;
+//         return object;
+//         }, {})
+//     };
+
+// let booksByTitle = groupByTitle(books)
+
+// const getTheTitles = groupByTitle(books);
+// console.table(getTheTitles);
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
